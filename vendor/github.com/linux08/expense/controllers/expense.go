c03c66bc125089f365fd2a5a648bbba5e7bf49c3
linux08 <abimbola130@gmail.com>
2018-12-24 16:06:51 +0100

    Debugging

--- vendor/github.com/linux08/expense/controllers/expense.go
+++ vendor/github.com/linux08/expense/controllers/expense.go
@@ -1,12 +1,32 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/linux08/expense/models"
 )
+
+//Init expense as a slice of expense struct
+var expense []models.Expense
+
+type expenses []models.Expense
+
+//intialized sample objec
+func InitSampleObj() expenses {
+	//Init expensest  var as a slice of expense struct
+	expense := append(expense,
+		models.Expense{ID: "1", Name: "good", Reason: "i need to save", Vat: "132",
+			User: &models.User{ID: "13", Name: "linx08", Gender: "male", Email: "abimbola120@gmail.com", Password: "pass123"}})
+	return expense
+}
 
 func GetExpenses(w http.ResponseWriter, r *http.Request) {
 	// fmt.Println("dddjj")
-	w.Write([]byte("Hello World"))
+	var expense = InitSampleObj()
+	fmt.Println("ed", expense)
+	json.NewEncoder(w).Encode(expense)
 }
 
 func GetExpense(w http.ResponseWriter, r *http.Request) {
--- vendor/github.com/linux08/expense/routes/routes.go
+++ vendor/github.com/linux08/expense/routes/routes.go
@@ -1,14 +1,22 @@
 package routes
 
 import (
+	"expense/models"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/linux08/expense/controllers"
 )
 
+//Init expense as a slice of expense struct
+var expense []models.Expense
+
 // Handlers : Use gorilla mux.
 // CRUD + Vote Handlers.
 func Handlers() *mux.Router {
+
 	r := mux.NewRouter().StrictSlash(true)
+	r.Use(commonMiddleware)
 
 	r.HandleFunc("/api", controllers.TestAPI).Methods("GET")
 	r.HandleFunc("/api/expenses", controllers.GetExpenses).Methods("GET")
@@ -17,4 +25,12 @@ func Handlers() *mux.Router {
 	r.HandleFunc("/api/expenses/{id}", controllers.UpdateExpense).Methods("PUT")
 	r.HandleFunc("/api/expenses/{id}", controllers.DeleteExpense).Methods("DELETE")
 	return r
+}
+
+//set content-type
+func commonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
