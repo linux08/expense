f089471810fb07fead55bf901015b60a1abb9081
linux08 <abimbola130@gmail.com>
2018-12-24 14:30:13 +0100

    Debugging

--- controllers/expense.go
+++ controllers/expense.go
@@ -1,31 +1,30 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func getExpenses(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dddjj")
+	// fmt.Println("dddjj")
 	w.Write([]byte("Hello World"))
 }
 
 func getExpense(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dddjj")
+	// fmt.Println("dddjj")
 	w.Write([]byte("Hello World"))
 }
 
 func createExpense(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dddjj")
+	// fmt.Println("dddjj")
 	w.Write([]byte("Hello World"))
 }
 
 func updateExpense(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dddjj")
+	// fmt.Println("dddjj")
 	w.Write([]byte("Hello World"))
 }
 
 func deleteExpense(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dddjj")
+	// fmt.Println("dddjj")
 	w.Write([]byte("Hello World"))
 }
--- controllers/user.go
+++ controllers/user.go
@@ -1,11 +1,10 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func testAPI(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("API live and kicking")
+	// fmt.Println("API live and kicking")
 	w.Write([]byte("API live and kicking"))
 }
